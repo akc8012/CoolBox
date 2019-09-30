9707f8a767e54c952eeb557571e4afa9d965ec23
Andrew Colannino <akc8012@gmail.com>
2019-09-30 16:08:26 -0400

    added dank things

---
+++ src/DankThings.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CoolBox
+{
+	// Put all dank things here
+	public class DankThings
+	{
+		public void PrintDank() => Console.WriteLine("Dank!!");
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,8 @@ namespace CoolBox
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var dank = new DankThings();
+			dank.PrintDank();
 		}
 	}
 }
